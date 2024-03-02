49575c92f494a33d84c229ff510a2c89e30d0526
KalyanMurapaka45 <kalyanmurapaka274@gmail.com>
2024-03-02 09:59:26 +0530

    Project Setup & Requirements Update

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US_Visa_Prediction",
+    version="0.0.1",
+    author="Hema Kalyan",
+    author_email="kalyanmurapaka274@gmail.com",
+    packages=find_packages()
+)
